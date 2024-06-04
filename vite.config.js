f3430b97b6924543fd920a02cd3323e5e4e1eae9
LIDERWAR <xx-serg-xx2011@yandex.ru>
2024-06-04 22:34:13 +0300

    Added work with html and images

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
 import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import injectHTML from 'vite-plugin-html-inject';
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
 
 export default defineConfig({
   root: './src',
@@ -9,5 +12,23 @@ export default defineConfig({
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main:'./src/index.html',
+        about:'./src/about.html',
+      }
+    }
   },
+  plugins: [ViteImageOptimizer({
+    png: {
+      quality: 80,
+    },
+    jpeg: {
+      quality: 80,
+    },
+
+  }),
+],
+  plugins: [injectHTML()],
+  plugins: [ViteMinifyPlugin()],
 });
