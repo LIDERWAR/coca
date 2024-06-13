b70acfbe7a405128dab09bc97e72cd080a44d932
LIDERWAR <xx-serg-xx2011@yandex.ru>
2024-06-13 21:01:10 +0300

    Created insight section and swiper slider

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,8 @@ import '../assets/scss/main.scss';
 
 import { useTheme } from './components/theme';
 import { useBurger } from './components/burger';
+import { useInsightSlider } from './components/slider';
 
 useTheme();
 useBurger();
+useInsightSlider();
