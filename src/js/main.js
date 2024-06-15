998d332b5dc5e32afbf4ec6c37c98f537418750f
LIDERWAR <xx-serg-xx2011@yandex.ru>
2024-06-15 14:17:35 +0300

    Created section testimonials

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,12 @@ import '../assets/scss/main.scss';
 
 import { useTheme } from './components/theme';
 import { useBurger } from './components/burger';
-import { useInsightSlider } from './components/slider';
+import {
+  useInsightSlider,
+  useTestimonialsSlider,
+} from './components/home/slider';
 
 useTheme();
 useBurger();
 useInsightSlider();
+useTestimonialsSlider();
